c801a39a14d4ca241b95e2399d8998652ea55afa
denismnjima <denismnjima@gmail.com>
2024-12-27 14:23:01 +0300

    adds carData

---
+++ src/Data/cardata.js
@@ -0,0 +1,24 @@
+export default  carData = [
+    {
+        image: 'fordRanger.png',
+        name:'Fordranger',
+        type:'pickup truck',
+        day_price: 5000,
+        seaters: 2
+    },
+    {
+        image: 'nissan_x_trail.png',
+        name:'Fordranger',
+        type:'pickup truck',
+        day_price: 5000,
+        seaters: 2
+    },
+    {
+        image: 'toyota_corrola.png',
+        name:'Toyota Corolla',
+        type:'Sedan',
+        day_price: 2700,
+        seaters: 4
+    }
+    
+]
