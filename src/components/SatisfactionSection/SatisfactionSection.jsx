59a40899128835722841da50dcbe87c5d4f53745
denismnjima <denismnjima@gmail.com>
2024-12-27 15:46:13 +0300

    adds image to satisfaction container

--- src/components/SatisfactionSection/SatisfactionSection.jsx
+++ src/components/SatisfactionSection/SatisfactionSection.jsx
@@ -1,8 +1,8 @@
-
+import carsImage from '../../assets/images/two_cars.png';
 function SatisfactionSection() {
   return (
     <section className="satisfaction-container">
-        <img src="" alt="" />
+        <img src={carsImage} alt="mistway limited" />
         <div className="details">
             <h2>Your satisfaction is our <span>main aim</span></h2>
             <p>At Mistway Limited delivering the best experience possible. Our team is dedicated to ensuring that every step of your journey with us exceeds expectations. Whether you're booking your first car rental or are a regular customer, we're here to make your experience smooth and hassle-free. We listen to your needs, provide exceptional service, and go above and beyond to make sure you're satisfied every time.
