860f3db08013213cc33e4aa655cdba7a34856d2b
denismnjima <denismnjima@gmail.com>
2024-12-27 13:55:40 +0300

    connects carCollection with CarCollectionContainer

---
+++ src/components/CarCollectionContainer/CarCollectionContainer.jsx
@@ -0,0 +1,9 @@
+import CarCollection from '../CarCollection/CarCollection'
+import './CarCollectionContainer.css'
+export default function CarCollectionContainer() {
+  return (
+    <div>
+        <CarCollection/>
+    </div>
+  )
+}
