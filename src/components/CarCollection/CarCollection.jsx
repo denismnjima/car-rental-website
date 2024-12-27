d3413647aaacd2cd5716711e177053700801cbb8
denismnjima <denismnjima@gmail.com>
2024-12-27 14:28:28 +0300

    creates CarCollection jsx

--- src/components/CarCollection/CarCollection.jsx
+++ src/components/CarCollection/CarCollection.jsx
@@ -1,7 +1,18 @@
+import { FaWhatsapp } from 'react-icons/fa'
 import './CarCollection.css'
+import { PiSeat } from 'react-icons/pi'
+
 function CarCollection() {
   return (
-    <div>CarCollection</div>
+    <div className='carCollectionInd'>
+      <div className="details">
+        <h2>Nissan X-Trail</h2>
+        <p>sedan</p>
+        <span><PiSeat/> 4</span>
+        <a href=""> <FaWhatsapp/> Book now</a>
+      </div>
+      
+    </div>
   )
 }
 
