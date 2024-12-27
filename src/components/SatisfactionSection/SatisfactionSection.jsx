996036c511485bc61b96e72a5340a756781b92ef
denismnjima <denismnjima@gmail.com>
2024-12-27 15:42:41 +0300

    adds content for satisfaction-container

---
+++ src/components/SatisfactionSection/SatisfactionSection.jsx
@@ -0,0 +1,15 @@
+
+function SatisfactionSection() {
+  return (
+    <section className="satisfaction-container">
+        <img src="" alt="" />
+        <div className="details">
+            <h2>Your satisfaction is our <span>main aim</span></h2>
+            <p>At Mistway Limited delivering the best experience possible. Our team is dedicated to ensuring that every step of your journey with us exceeds expectations. Whether you're booking your first car rental or are a regular customer, we're here to make your experience smooth and hassle-free. We listen to your needs, provide exceptional service, and go above and beyond to make sure you're satisfied every time.
+                Your comfort, convenience, and satisfaction are at the heart of everything we do. Trust us to be the reliable choice you can count on, time and time again.</p>
+        </div>
+    </section>
+  )
+}
+
+export default SatisfactionSection
